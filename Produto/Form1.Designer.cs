a4aba068403b24c98380df44c293b1992e9e819e
Stephani Boechat <stephani_boechat@hotmail.com>
2020-04-01 21:41:33 -0300

    first commit

---
+++ Produto/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Produto
+{
+    partial class tabelaProd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntCommit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bntCommit
+            // 
+            this.bntCommit.Location = new System.Drawing.Point(380, 270);
+            this.bntCommit.Name = "bntCommit";
+            this.bntCommit.Size = new System.Drawing.Size(107, 31);
+            this.bntCommit.TabIndex = 2;
+            this.bntCommit.Text = "Commit";
+            this.bntCommit.UseVisualStyleBackColor = true;
+            this.bntCommit.Click += new System.EventHandler(this.bntCommit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(400, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 232);
+            this.textBox1.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(97, 270);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(75, 31);
+            this.selectButton.TabIndex = 4;
+            this.selectButton.Text = "Select file";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(394, 232);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // tabelaProd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(591, 313);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bntCommit);
+            this.Name = "tabelaProd";
+            this.Text = "tabelaProd";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bntCommit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Produto/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Produto
+{
+    public partial class tabelaProd : Form
+    {
+        public tabelaProd()
+        {
+            InitializeComponent();
+            
+            
+        }
+        
+        private void bntCommit_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < tabelaProd.Rows.Count; i++)
+            {
+                var linha = tabelaProd.Rows[i];
+                for (int j = 0; j < tabelaProd.Rows[i].Cells.Count; j++)
+                {   
+                    var celula = linha.Cells[j];
+                    textBox1.Text += $"{celula.Value} ";
+                }
+                textBox1.Text += "\r\n" ;
+            }
+        }
+
+        //private void tabelaProd_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //{   var linhaindex = e.RowIndex;
+        //    var linha = tabelaProd.Rows[linhaindex];
+        //    for (int j = 0; j < linha.Cells.Count; j++)
+        //    {
+        //        var celula = linha.Cells[j];
+        //        textBox1.Text += $"{celula.Value} ";
+        //    }
+        //    textBox1.Text += "\r\n";
+        //}
+        public void OpenFileDialogForm()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            selectButton = new Button
+            {
+                Size = new Size(100, 20),
+                Location = new Point(15, 15),
+                Text = "Select file"
+            };
+            selectButton.Click += new EventHandler(SelectButton_Click);
+            textBox1 = new TextBox
+            {
+                Size = new Size(300, 300),
+                Location = new Point(15, 40),
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical
+            };
+            ClientSize = new Size(330, 360);
+            Controls.Add(selectButton);
+            Controls.Add(textBox1);
+        }
+        public void DateGrid(string path)
+        {
+            //Criando um DataTable
+            DataTable dt = new DataTable();
+
+            //Lendo Todas as linhas do arquivo CSV
+            string[] Linha = System.IO.File.ReadAllLines($"{path}");
+            //Neste For, vamos percorrer todas as linhas que foram lidas do arquivo CSV
+            for (Int32 i = 0; i < Linha.Length; i++)
+            {
+                //Aqui Estamos pegando a linha atual, e separando os campos
+                //Por exemplo, ele vai lendo um texto, e quando achar um ponto e virgula
+                //ele pega o texto e joga na outra posição do array temp, e assim por diante
+                //até chegar no final da linha
+                string[] campos = Linha[i].Split(Convert.ToChar(";"));
+
+                //Um datable precisa de colunas
+                //Como a função acima jogou cada campo em uma posição do array de acordo com
+                //o ponto e virgula, eu estou pegando quantos campos ele achou e criando a mesma
+                //quantidade de colunas no datatable para corresponder a cada campo
+                if (i == 0)
+                {
+                    for (Int32 i2 = 0; i2 < campos.Length; i2++)
+                    {
+                        //Criando uma coluna
+                        DataColumn col = new DataColumn();
+                        col.ColumnName = campos[i2];
+                        col.Namespace = campos[i2];
+                        //Adicionando a coluna criada ao datatable
+                        dt.Columns.Add(col);
+                    }
+                }
+                if (i > 0)
+                {
+                    dt.Rows.Add(campos);
+                }
+                //Inserindo uma linha(row) no datable (Vai fazer isso para cada linha encontrada
+                //no arquivo CSV
+
+            }
+
+            //Depois de montado o datatable, vamos falar para o grid
+            //que a fonte de dados para ele exibir, será o datatable que 
+            //a gente acabou de criar
+
+            tabelaProd.DataSource = dt;
+        }
+        //private void SetText(string text)
+        //{
+        //    textBox1.Text = text;
+        //}
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var path = openFileDialog1.FileName;
+                    DateGrid(path);
+                    //var sr = new StreamReader(openFileDialog1.FileName);
+                    //SetText(sr.ReadToEnd());
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+    }
+}
